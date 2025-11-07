31bfaeed92ef7c3132f56e1a013923f1bc637fc4
muhammad <olmospro1978@gmail.com>
2025-11-07 22:15:49 +0500

    first commit

--- src/main/java/uz/pdp/g57jakarta_ee/MyFirstServlet.java
+++ src/main/java/uz/pdp/g57jakarta_ee/MyFirstServlet.java
@@ -14,7 +14,28 @@ public class MyFirstServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.println("Salom Ahmadulloh!");
+
+        resp.setContentType("text/html");
+//        writer.println("<h1>Salom Ahmadulloh!</h1>");
+//        writer.println("<button>ok</button>");
+
+        writer.println("""
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Title</title>
+                </head>
+                <body>
+                
+                <form>
+                  <input type="text">
+                </form>
+                
+                </body>
+                </html>
+               
+                """);
     }
 
 }
