de4e1cc2a4a90a2c6513647d25c09767bd9cbc1e
muhammad <olmospro1978@gmail.com>
2025-11-07 18:09:39 +0500

    first commit

---
+++ src/main/java/uz/pdp/g57jakarta_ee/MyFirstServlet.java
@@ -0,0 +1,20 @@
+package uz.pdp.g57jakarta_ee;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFirstServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Salom Ahmadulloh!");
+    }
+
+}
