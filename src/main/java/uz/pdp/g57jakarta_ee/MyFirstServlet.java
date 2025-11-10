dfcc5ee59633319f5c5dc431d3b1090615444a7c
muhammad <olmospro1978@gmail.com>
2025-11-10 18:02:25 +0500

    bugungi

--- src/main/java/uz/pdp/g57jakarta_ee/MyFirstServlet.java
+++ src/main/java/uz/pdp/g57jakarta_ee/MyFirstServlet.java
@@ -1,7 +1,7 @@
 package uz.pdp.g57jakarta_ee;
 
-import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,33 +9,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet({"/hello", "/salom", "/assalom"})
 public class MyFirstServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
         PrintWriter writer = resp.getWriter();
-
-        resp.setContentType("text/html");
-//        writer.println("<h1>Salom Ahmadulloh!</h1>");
-//        writer.println("<button>ok</button>");
-
         writer.println("""
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Title</title>
-                </head>
-                <body>
                 
-                <form>
-                  <input type="text">
-                </form>
-                
-                </body>
-                </html>
-               
                 """);
     }
 
+
+
+
+
 }
+
+
+// API -> (request) UNIQUE NAME, REQUEST BODY(optional), REQUEST PARAM(optional), HEADER, METHOD     ->  <- RESPONSE, HEADER, STATUS
